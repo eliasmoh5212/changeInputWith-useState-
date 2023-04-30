69930def052ca64ab6c0d6a6fe1f53d75b7ec685
eliasmoh5212 <eliasmoh5212@gamil.com>
2023-04-30 15:30:48 +0430

    2nd change to Form.tsx

--- src/Form.tsx
+++ src/Form.tsx
@@ -10,7 +10,7 @@ const Form = () => {
     event.preventDefault();
     if (paragraph != null) {
       paragraph.innerHTML =
-        "Dear " + person.name + " with the age of " + person.age;
+        "Dear Mr" + person.name + " with the age of " + person.age;
     }
   };
 
