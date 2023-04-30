275ecc57e60e4b0cd8e3eb22e6c23ac749c36f38
eliasmoh5212 <eliasmoh5212@gamil.com>
2023-04-30 15:21:52 +0430

    Added first changes

---
+++ src/Form.tsx
@@ -0,0 +1,56 @@
+import { FormEvent, useRef, useState } from "react";
+
+const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
+  let paragraph = document.getElementById("para");
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (paragraph != null) {
+      paragraph.innerHTML =
+        "Dear " + person.name + " with the age of " + person.age;
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          value={person.name}
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          value={person.age}
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+      <hr />
+      <p id="para"></p>
+    </form>
+  );
+};
+
+export default Form;
